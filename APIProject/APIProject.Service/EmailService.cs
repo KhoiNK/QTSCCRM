4707b72e464ce5901c8c3a29bb71cf4403b2d971
Phong <tranloiphong2@gmail.com>
2017-10-14 06:45:57 +0700

    Create simple email service

---
+++ APIProject/APIProject.Service/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject.Service
+{
+    class EmailService : IEmailService
+    {
+        /// <summary>
+        /// Send email
+        /// </summary>
+        /// <param name="message">
+        /// new MailMessage(
+        ///         "senderEmail@gmail.com",
+        ///         "receiverEmail@gmail.com",
+        ///         "TestCRMSubject",
+        ///         "TestCRMBody"
+        ///       );
+        /// </param>
+        /// <param name="credential">
+        /// new NetworkCredential(
+        ///         "senderEmail@gmail.com",
+        ///         "senderPassword"
+        ///     );
+        /// </param>
+        public void SendEmail(MailMessage message, NetworkCredential credential)
+        {
+            SmtpClient smtpobj = new SmtpClient("smtp.gmail.com", 25)
+            {
+                EnableSsl = true,
+                Credentials = credential
+            };
+            smtpobj.Send(message);
+        }
+    }
+
+    public interface IEmailService
+    {
+        void SendEmail(MailMessage message, NetworkCredential credential);
+    }
+}
