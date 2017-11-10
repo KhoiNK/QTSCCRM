692ef77b4568b7c92208a4ceb8f3139c81da3fb1
Phong <tranloiphong2@gmail.com>
2017-11-10 09:38:39 +0700

    Add build quote email funtion for email service

--- APIProject/APIProject.Service/EmailService.cs
+++ APIProject/APIProject.Service/EmailService.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using APIProject.Model.Models;
+using DotLiquid;
 
 namespace APIProject.Service
 {
     class EmailService : IEmailService
     {
+        private NetworkCredential GetDefaultNetworkCredential()
+        {
+            return null;
+        }   
         /// <summary>
         /// Send email
         /// </summary>
@@ -35,13 +42,47 @@ namespace APIProject.Service
                 Credentials = credential
             };
 
-            
             smtpobj.Send(message);
+        }
+
+        public MailMessage BuildQuoteInfomationBody(MailMessage message, string customerName, Staff staff,
+            List<SalesItem> salesItems,
+            Quote quote)
+        {
+            if (message == null)
+            {
+                message = new MailMessage();
+            }
+            string file = AppDomain.CurrentDomain.BaseDirectory + "/EmailTemplate/PriceEmailTemplate.html";
+            string templateString = File.ReadAllText(file);
+            Template template = Template.Parse(templateString);
+            var objects = salesItems.Select(item => Hash.FromAnonymousObject(new
+            {
+                item.Name,
+                item.Price,
+                item.Unit
+            }));
+            string body = template.Render(Hash.FromAnonymousObject(new
+            {
+                customerName,
+                salesItems = objects,
+                tax = quote.Tax,
+                discount = quote.Discount,
+                staffName = staff.Name,
+                staffEmail = staff.Email,
+                staffPhonenumber = staff.Phone
+            }));
+            message.Body = body;
+            message.IsBodyHtml = true;
+            return message;
         }
     }
 
     public interface IEmailService
     {
         void SendEmail(MailMessage message, NetworkCredential credential);
+
+        MailMessage BuildQuoteInfomationBody(MailMessage message, string customerName, Staff staff,
+            List<SalesItem> salesItems, Quote quote);
     }
 }
