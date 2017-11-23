775a419f7d53cc7c13325a111d0b80c4c0831743
Phong <tranloiphong2@gmail.com>
2017-11-23 15:34:01 +0700

    Update on compare service

--- APIProject/APIProject.Service/CompareService.cs
+++ APIProject/APIProject.Service/CompareService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace APIProject.Service
@@ -11,15 +12,27 @@ namespace APIProject.Service
     public interface ICompareService
     {
         IEnumerable<Customer> GetSimilarCustomers(Customer customer);
-        double StringCompare(string a, string b);
     }
-    public class CompareService:ICompareService
+
+    public class CompareService : ICompareService
     {
         private readonly ICustomerRepository _customerRepository;
+
+        private readonly string[] irrelevantWords =
+        {
+            "CÔNG TY", "CONG TY", "TNHH", "TRÁCH NHIỆM", "HỮU HẠN", "CTY", "COMPANY", "INC", "CỔ PHẦN",
+            "CP", "THƯƠNG MẠI", "THUONG MAI", "TM", "DỊCH VỤ", "DICH VU", "DV", "XUẤT NHẬP KHẨU",
+            "XUAT NHAP KHAU", "XNK", "TM&DV", "MỘT THÀNH VIÊN", "MOT THANH VIEN", "MTV", "ĐẦU TƯ",
+            "DAU TU", "PHÁT TRIỂN", "PHAT TRIEN", "VÀ", "NỘI THẤT", "NOI THAT", "KINH DOANH",
+            "KINH DOANH", "XÂY DỰNG", "XAY DUNG", "DU LỊCH", "DU LICH", "TRUYỀN THÔNG", "TRUYEN THONG",
+            "CÔNG NGHỆ", "CONG NGHE", "TƯ NHÂN"
+        };
+
         public CompareService(ICustomerRepository _customerRepository)
         {
             this._customerRepository = _customerRepository;
         }
+
         public double StringCompare(string a, string b)
         {
             if (a == b) //Same string, no iteration needed.
@@ -41,11 +54,28 @@ namespace APIProject.Service
             return sameCharAtIndex / maxLen * 100;
         }
 
+        public string FilterIrrelevantWords(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            input = input.ToUpper();
+            input = Regex.Replace(input, @"\s+", " ");
+            foreach (string irrelevantWord in irrelevantWords)
+            {
+                input = input.Replace(irrelevantWord, "");
+            }
+            return input.Trim();
+        }
+
         public IEnumerable<Customer> GetSimilarCustomers(Customer customer)
         {
             int percentage = 70;
-            var customerEntities = _customerRepository.GetAll().Where(c => c.IsDelete == false
-            && StringCompare(customer.Name,c.Name)>=percentage);
+            var customerEntities = _customerRepository.GetAll()
+                .Where(c => c.IsDelete == false &&
+                            StringCompare(FilterIrrelevantWords(customer.Name), FilterIrrelevantWords(c.Name)) >=
+                            percentage);
             return customerEntities;
         }
     }
